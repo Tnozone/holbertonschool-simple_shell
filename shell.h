7fd0d2710dc249a06bba234b6e91e1eeb14718c4
Your Name <as.segueg2501@gmail.com>
2022-11-29 02:56:08 -0800

    new prototype

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+
+#endif
