66d62ea561720d7b69e769f8fd01b6d45b1e0405
Your Name <as.segueg2501@gmail.com>
2022-11-29 04:36:16 -0800

    main_shell.c

--- main_shell.c
+++ main_shell.c
@@ -6,3 +6,14 @@ int main(__attribute__((unused))int argc, __attribute__((unused))char **argv)
 
 	return (EXIT_SUCCESS);
 }
+#include "shell.h"
+char *builtin_str[] =
+{
+  "cd",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &func_cd,
+  &exit_pro
+};
