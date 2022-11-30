03dde52cea8747b1764e2da82db006786f1916a1
Your Name <as.segueg2501@gmail.com>
2022-11-30 00:46:23 -0800

    control if correct saisi

---
+++ func_control.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+/**
+ * control_saisi - That control if saisi exists.
+ * @saisi: command entered by user
+ * Return: (NULL)
+ */
+int (*control_saisi(char *saisi))(char *cmd, char **args, char **env)
+{
+	int i;
+	control_t tbs[] = {
+		{"exit", func_exit},
+		{"env", print_env},
+		{NULL, NULL},
+	};
+
+	for (i = 0; tbs[i].saisi != NULL; i++)
+	{
+		if (strcmp(saisi, tbs[i].saisi) == 0)
+			return (tbs[i].f);
+	}
+	return (NULL);
+}
