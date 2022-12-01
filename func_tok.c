0a841e1f14af210cb13764eb3e931bd52de2c376
Your Name <as.segueg2501@gmail.com>
2022-12-01 05:49:59 -0800

    Unfreed Memory Repair

--- func_tok.c
+++ func_tok.c
@@ -33,27 +33,4 @@ char **func_tok(char *buf)
 	tokens[i] = NULL;
 	return (tokens);
 }
-/**
-char **func_tok(char *buff)
-{
-	int size = BUFSIZE;
-	char **tokens = NULL;
-	char *arg = NULL;
-	int i = 0;
-
-	tokens = malloc(size * sizeof(char*));
-	if (!tokens)
-		return (NULL);
-	arg = strtok(buff, DELIM);
-
-	while (arg != NULL)
-	{
-		tokens[i] = strdup(arg);
-		arg = strtok(NULL, DELIM);
-		i++;
-	}
-	tokens[i] = NULL;
-	return (tokens);
-}
-*/
 
