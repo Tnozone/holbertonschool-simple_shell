fc3a701c4114bf5748f0e07fa3d0ecce5ccc1e87
Your Name <as.segueg2501@gmail.com>
2022-12-03 09:14:26 -0800

    Clear

--- shell.h
+++ shell.h
@@ -4,6 +4,15 @@
 #define IN 1
 #define OUT 0
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 extern char **environ;
 
 typedef struct control
@@ -12,25 +21,14 @@ typedef struct control
 	int (*f)(char *cmd, char **args, char **env);
 } control_t;
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
 
-void free_tab(char **tabtok);
 void print_pr(void);
 int start(char *buff, char **env, int line);
+int (*control_saisii(char *saisi))(char *cmd, char **args, char **env);
+void free_tab(char **tabtok);
 int exec_path(char **args, char **env, int line);
 char *path_func(char *cmd, char *PATH);
-int (*control_saisii(char *saisi))(char *cmd, char **args, char **env);
 int func_exit(char *cmd, char **args, char **env);
 char **func_tok(char *buf);
-size_t numberchar(char *s);
-void free_tokens(char **tokens);
-int print_env(char *comande, char **arg, char **envi);
-void func_signal(__attribute__((unused)) int signal);
 
 #endif
