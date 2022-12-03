8416c52a48b63c186655b32b37b605d829049242
Your Name <as.segueg2501@gmail.com>
2022-12-03 09:01:21 -0800

    Betty coding

--- shell.h
+++ shell.h
@@ -20,7 +20,7 @@ typedef struct control
 #include <sys/wait.h>
 #include <signal.h>
 
-void free_tab (char **tabtok);
+void free_tab(char **tabtok);
 void print_pr(void);
 int start(char *buff, char **env, int line);
 int exec_path(char **args, char **env, int line);
