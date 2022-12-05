50afe8ed5b3060ea863a93fa787a54c91963aa40
Tnozone <maxnocquet@gmail.com>
2022-12-05 01:20:03 -0800

    strcat function

---
+++ _strcat.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * _strcat - Entry point
+ * @dest: destination buffer
+ * @src: source string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+  int y = 0, z = 0, len = 0;
+
+  while (src[len] != '\0')
+    len++;
+
+  while (dest[z] != '\0')
+    z++;
+
+  len += z;
+
+  while (z <= len)
+    {
+      dest[z] = src[y];
+      y++;
+      z++;
+    }
+  return (dest);
+}
